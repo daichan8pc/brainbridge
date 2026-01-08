a8875aaa2ce7a8f2b21aeb6b95f942227c52d0a5
daichan8pc <s25121@numazu.kosen-ac.jp>
2026-01-09 00:42:53 +0900

    Add: クラッシュチェックプログラムを作成・実行

---
+++ app/check_crash.py
@@ -0,0 +1,9 @@
+print("1. Start")
+import numpy
+print(f"2. NumPy Imported: {numpy.__version__}")
+import pandas
+print("3. Pandas Imported")
+import torch
+print(f"4. PyTorch Imported: {torch.__version__}")
+import streamlit
+print("5. Streamlit Imported")
